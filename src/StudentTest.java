84bece8bb8e6be6c0f15ed52dc041fa1e07a69a3
yaelBrown <yaelrbrown@gmail.com>
2019-09-30 11:48:04 -0500

    Setup project and completed 1.

---
+++ src/StudentTest.java
@@ -0,0 +1,17 @@
+public class StudentTest {
+
+    public static void main(String[] args) {
+
+        Student yael = new Student("yael");
+        
+        yael.addGrade(10);
+        yael.addGrade(20);
+        yael.addGrade(20);
+
+        System.out.println("yael.getGradeAverage() = " + yael.getGradeAverage());
+
+
+
+    }
+
+}
